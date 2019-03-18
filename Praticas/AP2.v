d2b1ffcdc7bb9abebc779b58f92873ad04a29136
unknown <francisco.duarte1995@gmail.com>
2019-03-18 15:42:59 +0000

    continuacao

--- Praticas/AP2.v
+++ Praticas/AP2.v
@@ -55,6 +55,8 @@ Compute skipn 3 [1;2;3;4;5].
 Inductive option (X :Type) : Type :=
   | Some : X -> option X
   | None : option X.
+
+
 Arguments Some {X} _.
 Arguments None {X}.
 
@@ -161,44 +163,136 @@ Compute find (fun e => e <=? 3) [6;4;4;5;7].
 
 (* 2.1. *)
 
-(* Duvida
-Fixpoint partition {X: Type} (test: X -> bool) (l: list X) : (list X) * (list X) :=
+Fixpoint filter {X:Type} (test: X->bool) (l:list X)
+                : (list X) :=
   match l with
   | [] => []
-  | h :: t => if test h then  
+  | h :: t => if test h then h :: (filter test t)
+                        else filter test t
+  end.
 
-*)
+Fixpoint partition {X: Type} (test: X -> bool) (l: list X) : (list X) * (list X) :=
+  (filter test l , filter (fun e => negb (test e)) l).
+
+Compute partition (fun e => e <=? 3) [6;4;1;3;7].
 
 (* 2.2. *)
 
-(*
+Fixpoint map {X Y: Type} (f:X->Y) (l:list X) : (list Y) :=
+  match l with
+  | [] => []
+  | h :: t => (f h) :: (map f t)
+  end.
 
-Fixpoint list_prod {X, Y: Type} (l1: list X) (l2: list Y) : list X*Y :=
+Fixpoint list_prod {X Y: Type} (l1: list X) (l2: list Y) : list (X*Y) :=
   match l1 with
-  | [] = []
-  | h1 :: t1 => match l2 with
-                | [] = []
-                | h2 :: t2 => (h1 , h2) :: listprod l1 t2
-                end
+  | [] => []
+  | h1 :: t1 => (map (fun e => (h1 , e)) l2) ++ (list_prod t1 l2)
+end.
 
-*)
+(* Duvida *)
+
+Compute list_prod [1; 2] [true; false].
+
+
 (* 2.3. *)
+
+Fixpoint fold {X Y: Type} (f: X->Y->Y) (l: list X) (b: Y) : Y :=
+  match l with
+  | nil => b
+  | h :: t => f h (fold f t b)
+  end.
+
+Definition length {X : Type} (l: list X) : nat :=
+  fold (fun e c => c + 1) l 0.
+
+Compute length [6;4;1;3;7].
 
 (* 2.4. *)
 
+Definition new_map {X Y: Type} (f:X->Y) (l:list X) : (list Y) :=
+  fold (fun x y => f x :: y) l [].
+
+
+Fixpoint list_prod' {X Y: Type} (l1: list X) (l2: list Y) : list (X*Y) :=
+  match l1 with
+  | [] => []
+  | h1 :: t1 => (new_map (fun e => (h1 , e)) l2) ++ (list_prod t1 l2)
+end.
+
+Compute list_prod' [1; 2] [true; false].
+
 (* 2.5. *)
+
+Definition new_filter {X:Type} (test: X->bool) (l:list X): (list X) :=
+  fold (fun x y => if test x then x :: y else y ) l [].
+
+Fixpoint partition' {X: Type} (test: X -> bool) (l: list X) : (list X) * (list X) :=
+  (filter test l , new_filter (fun e => negb (test e)) l).
+
+Compute partition' (fun e => e <=? 3) [6;4;1;3;7].
 
 (* 2.6. *)
 
+Check andb.
+
+Definition new_forallb {X: Type} (test: X -> bool) (l: list X) : bool :=
+  fold andb (map test l) true.
+
+Compute new_forallb (fun e => e <=? 3) [1;2;3].
+Compute new_forallb (fun e => e <=? 3) [1;2;4].
 
 (* Exercise 3 *)
 
 (* 3.1. *)
 
+Theorem thm_simpl1: forall a b c:nat,
+    a = 0 -> b*(a+b) = b*b.
+Proof.
+  intros.
+  rewrite -> H.
+  simpl.
+  reflexivity.
+Qed.
+
 (* 3.2. *)
+
+Theorem thm_simpl2: forall (a b c d:nat) (f: nat -> nat -> nat),
+    a=b -> c=d -> (forall x y, f x y = f y x) -> f a c = f d b.
+Proof.
+  intros.
+  rewrite -> H1.
+  rewrite -> H.
+  rewrite -> H0.
+  reflexivity.
+Qed.
 
 (* 3.3. *)
 
+Theorem identity_fn_applied_twice :
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = x) ->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros.
+  rewrite H.
+  rewrite H.
+  reflexivity.
+Qed.
+
 (* 3.4. *)
+
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = negb x)->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros.
+  rewrite H. rewrite H.
+  
+  rewrite identity_fn_applied_twice.
+  -reflexivity.
+  - intros.
+    rewrite H.
 
 
